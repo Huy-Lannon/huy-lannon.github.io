05f6011edb04981f304172c7a2292f629665e179
Huy-Lannon <stephenpapale@gmail.com>
2026-02-01 22:58:47 +0200

    Initial upload - Between Lines app

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'between-lines-v1';
+const urlsToCache = [
+  '/',
+  '/literary-app.html',
+  'https://fonts.googleapis.com/css2?family=Crimson+Pro:wght@300;400;500&family=Work+Sans:wght@300;400;500&display=swap'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Update service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
